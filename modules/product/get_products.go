e09bc9bab6d9cfbd7893cb22fb7e1b56062bac8c
Riaz Fahad <r.fahad@iqfulfillment.com>
2026-03-25 22:19:55 +0600

    waitgroup added

--- modules/product/get_products.go
+++ modules/product/get_products.go
@@ -2,9 +2,14 @@ package product
 
 import (
 	"ecommerce/util"
+	"log"
 	"net/http"
 	"strconv"
+	"sync"
+	"time"
 )
+
+var count int
 
 func (h *Handler) GetProducts(w http.ResponseWriter, r *http.Request) {
 
@@ -26,16 +31,65 @@ func (h *Handler) GetProducts(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	count, err := h.productService.Count()
-	if err != nil {
-		util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// if len(list) <= 0 {
-	// 	util.ErrorResponse(w, "Empty products", http.StatusNotFound)
+	// count, err := h.productService.Count()
+	// if err != nil {
+	// 	util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
 	// 	return
 	// }
+
+	var wg sync.WaitGroup
+
+	wg.Add(4)
+	go func() {
+		defer wg.Done()
+
+		time.Sleep(7 * time.Second)
+		count1, err := h.productService.Count()
+		if err != nil {
+			util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		count = count1
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		time.Sleep(7 * time.Second)
+		count2, err := h.productService.Count()
+		if err != nil {
+			util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		log.Println(count2)
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		time.Sleep(7 * time.Second)
+		count3, err := h.productService.Count()
+		if err != nil {
+			util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		log.Println(count3)
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		time.Sleep(7 * time.Second)
+		count4, err := h.productService.Count()
+		if err != nil {
+			util.ErrorResponse(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		log.Println(count4)
+
+	}()
+
+	wg.Wait()
 
 	util.SendPaginatedData(w, r, list, page, limit, count)
 }
